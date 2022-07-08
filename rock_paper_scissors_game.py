d1a89bdafa1dd3960a17be02795f12124045ffca
Ömer Rençbereli <omerrencbereli@gmail.com>
2022-07-08 18:02:14 +0300

    Starting

---
+++ rock_paper_scissors_game.py
@@ -0,0 +1,7 @@
+"""
+Hello World
+This is our first git project by Omer and Erdem Rencbereli
+"""
+liste = ["rock","paper","scissors"]
+round = int(input("This is a rock-paper-scissors game"))
+starting_round = 0
