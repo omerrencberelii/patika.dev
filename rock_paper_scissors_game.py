25eaac6dfe007d6e1660897678fc8258ec8387b9
omerrencberelii <105645536+omerrencberelii@users.noreply.github.com>
2022-11-17 19:47:17 +0300

    Delete rock_paper_scissors_game.py

--- rock_paper_scissors_game.py
+++
@@ -1,7 +0,0 @@
-"""
-Hello World
-This is our first git project by Omer and Erdem Rencbereli
-"""
-liste = ["rock","paper","scissors"]
-round = int(input("This is a rock-paper-scissors game"))
-starting_round = 0
